3cd4c410604a7218203e6389329145fb85580d77
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 19:09:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,14 @@
-# def prime?(value)
-#   if value <= 1 || value == 0 || value == 1
-#     return false
-#   elsif
-#     (2..value - 1).each do |i|
-#       if value % i == 0
-#       return false
-#     end
-#   end
-# end
-# true
-# end
-
 def prime?(value)
-  return false if value <= 1
-else
+  if value <= 1 || value == 0 || value == 1
+    return false
+  elsif
     (2..value - 1).each do |i|
       if value % i == 0
-        return false
+      return false
+      end
     end
   end
-  true  # <= need explanation
+  true
 end
+
+
