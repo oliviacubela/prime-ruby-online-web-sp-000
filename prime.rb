72a034fb0a58bbf9d2eb51b3b0f0a746ead1f05d
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 19:03:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
   return n | 1 == 3 if n < 5
   return false if n.gcd(6) != 1
-  p, sqrtn = 5, Integer.sqrt(n)           
+  p, sqrtn = 5, Integer.sqrt(n)
   until p > sqrtn
     return false if n % p == 0 || n % (p + 2) == 0
     p += 6
