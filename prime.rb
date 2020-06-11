d494aa936e83b978fa2517bf247e5bf7baa1384c
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 19:08:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,3 @@
-#
-# def prime?(n)
-#   return n | 1 == 3 if n < 5
-#   return false if n.gcd(6) != 1
-#   p, sqrtn = 5, Integer.sqrt(n)
-#   until p > sqrtn
-#     return false if n % p == 0 || n % (p + 2) == 0
-#     p += 6
-#   end
-#   true
-# end
 def prime?(value)
   if value <= 1 || value == 0 || value == 1
     return false
@@ -19,5 +8,16 @@ def prime?(value)
     end
   end
 end
-true  # <= need explanation
+true  
+end
+
+def prime?(value)
+  return false if value <= 1
+else
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+    end
+  end
+  true  # <= need explanation
 end
