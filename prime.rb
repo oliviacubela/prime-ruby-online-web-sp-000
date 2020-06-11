fdd9d9862d071283942c7710b108a28acc2db376
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 18:55:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+# def prime?( )
+# 
+# end
+def trial_division(n: int) -> List[int]:
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n /= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n /= f
+        else:
+            f += 2
+    if n != 1: a.append(n)
+    # Only odd number is possible
+    return a
