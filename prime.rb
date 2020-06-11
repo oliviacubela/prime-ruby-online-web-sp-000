4a8784e488e86d5ef5e76a351fac4d32fe22a874
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 19:01:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,15 @@
-def prime(n)
-  /'1'*n !~ /^1?$|^(11+?)\1+$//
+require "benchmark/ips"
+
+def primep3?(n)                           # P3 Prime Generator primality test
+  # P3 = 6*k + {5, 7}                     # P3 primes candidates (pc) sequence
+  return n | 1 == 3 if n < 5              # n: 0,1,4|false, 2,3|true
+  return false if n.gcd(6) != 1           # 1/3 (2/6) of integers are P3 pc
+  p, sqrtn = 5, Integer.sqrt(n)           # first P3 sequence value
+  until p > sqrtn
+    return false if n % p == 0 || n % (p + 2) == 0  # if n is composite
+    p += 6      # first prime candidate for next kth residues group
+  end
+  true
 end
 # def trial_division(n: int) -> List[int]:
 #     a = []
